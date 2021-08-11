2478819a4efa967276516e9e4737eb4ef94f7932
Rehan <Abusayid693@gmail.com>
2021-08-11 22:32:05 +0000

    #3 revolved and few code refactored

--- main.py
+++ main.py
@@ -8,6 +8,21 @@ client = discord.Client()
 sentiment_API = os.environ['SENTIMENT_API']
 paralleldots.set_api_key(sentiment_API)
 
+# Bot will only work on these channels
+Bot_Channels=[
+  857549642934124545,
+  857569486303264768,
+  857551907714760704,
+  870716941663350825,
+  857549120943292436,
+  857549492324139059,
+  863495216467804220,
+  874356441069269032,
+  874630819388473344
+]
+
+
+
 # "$" special character is to replace with user name
 words_response = [
     "Hi! $", "How are things with you $", 
@@ -69,34 +84,33 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    if message.channel.id not in Bot_Channels:
+       return
     text = message.content.lower().strip()
     if message.author == client.user:
         return
-
     if "limbo" in text:
         response_message = random.choice(words_response)
         user_name = message.author.name
         response_message = response_message.replace("$", user_name)
-        await message.channel.send(response_message)
+        await auto_response(True,message,response_message)
 
     if message.author.name in user_names:
        await message.add_reaction(Custom_emojis.get(message.author.name))
-
     # Direct links of limbohacks for easy access with '!' prefix
     await auto_response(text.startswith('!website'),message,"https://limbohacks.tech/")
     await auto_response(text.startswith('!devpost'),message,"https://limbo-hacks-12968.devpost.com/")
     await auto_response(text.startswith('!discord'),message,"https://discord.com/invite/8XJSzmtWPp")       
     #Getting message sentiment
-    if message.channel.id!=873538964689149963:
-      result = check_sentiment(text)
-      if result:    
-        await message.add_reaction(sentiment_emojis.get(result))
-        print(message.channel.id)
-
+    result = check_sentiment(text)
+    await auto_react(result,message,sentiment_emojis.get(result))
 
 
 async def auto_response(condition,message,content):
   if condition:
    await message.channel.send(content)
 
+async def auto_react(condition,message,content):
+  if condition:
+   await message.add_reaction(content)
 client.run(os.environ['TOKEN'])
